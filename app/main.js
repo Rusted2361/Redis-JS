f4d3dd09fe7b138d2bfaa7777b7732333698e1be
Rusted2361 <hamzasajid4040@gmail.com>
2024-04-17 17:39:15 +0500

    switch statement

--- app/main.js
+++ app/main.js
@@ -6,11 +6,21 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server = net.createServer((connection) => {
   // Handle multiple connection
-  connection.on("data", (data) => {
-    connection.write("+PONG\r\n");
-
-    // connection.end();
-  });
+  let args = data.toString().toLowerCase().split("\r\n");
+        console.log(args);
+        switch(args[2]){
+            case "echo":
+                console.log("Echoing");
+                connection.write(args[3] + "\r\n" + args[4] + "\r\n");
+                break;
+            case "ping":
+                console.log("Pinging");
+                connection.write("+PONG\r\n");
+                break;
+            default:
+                console.log("Command not found");
+1
+        }
   connection.on("end", () => {
 
     console.log("client disconnected");
