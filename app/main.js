a7d2c67bca547f666e5beb3f1f2ea1b0f2c0d54e
Rusted2361 <hamzasajid4040@gmail.com>
2024-04-18 17:38:18 +0500

    changed vars

--- app/main.js
+++ app/main.js
@@ -20,7 +20,9 @@ const server = net.createServer((connection) => {
                 break;
             case "set":
                 console.log("Set command");
-                map.set(args[4], args[6]);
+                const key = args[4]
+                const value = args[6]
+                map.set(key, value);
                 if (args[3]) {
                     const cmd = args[8];
                     console.log("cmd",cmd)
@@ -28,8 +30,8 @@ const server = net.createServer((connection) => {
                         const ms = parseInt(args[10]);
                         console.log("ms",ms);
                         setTimeout(() => {
-                            console.log('delete', args[4]);
-                            map.delete(args[4]);
+                            console.log('delete', key);
+                            map.delete(key);
                         }, ms);
                     }
     1
